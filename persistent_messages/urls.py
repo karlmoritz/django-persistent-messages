4ec9f034b6a5c67b36d75370d1afb765a3ad8eb1
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2010-11-12 17:06:13 +0100

    Adding urls for the new views

--- persistent_messages/urls.py
+++ persistent_messages/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-    url(r'^detail/(?P<message_id>\d+)/$', 'persistent_messages.views.message_detail', name='message_detail'),
-    url(r'^mark_read/(?P<message_id>\d+)/$', 'persistent_messages.views.message_mark_read', name='message_mark_read'),
-    url(r'^mark_read/all/$', 'persistent_messages.views.message_mark_all_read', name='message_mark_all_read'),
+urlpatterns = patterns('persistent_messages.views',
+    url(r'^detail/(?P<message_id>\d+)/$', 'message_detail', name='message_detail'),
+
+    # Mark read
+    url(r'^mark_read/(?P<message_id>\d+)/$', 'message_mark_read', name='message_mark_read'),
+    url(r'^mark_read/all/$', 'message_mark_all_read', name='message_mark_all_read'),
+    
+    # Delete
+    url(r'^borrar/mensaje/(?P<message_id>\d+)/$', 'message_delete', name='message_delete'),
+    url(r'^borrar/todos/$', 'message_delete_all', name='message_delete_all'),
 )
