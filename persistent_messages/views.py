4fd06d4f749ceb4a4c9791a25d6a5316f022d954
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2010-11-12 17:03:45 +0100

    Adding views for deleting messages: delete_message (deletes an specific message) delete_all (deletes all messages from the logged in user)

--- persistent_messages/views.py
+++ persistent_messages/views.py
@@ -9,28 +9,62 @@ def message_detail(request, message_id):
     user = get_user(request)
     if not user.is_authenticated():
         raise PermissionDenied
+
     message = get_object_or_404(Message, user=user, pk=message_id)
     message.read = True
     message.save()
+
     return render_to_response('persistent_messages/message/detail.html', {'message': message}, 
         context_instance=RequestContext(request))
 
-def message_mark_read(request, message_id):
+
+def message_delete(request, message_id):
     user = get_user(request)
     if not user.is_authenticated():
         raise PermissionDenied
+
     message = get_object_or_404(Message, user=user, pk=message_id)
-    message.read = True
-    message.save()
+    message.delete()
+
     if not request.is_ajax():
         return HttpResponseRedirect(request.META.get('HTTP_REFERER') or '/')
     else:
         return HttpResponse('')
 
+
+def message_delete_all(request):
+    user = get_user(request)
+    if not user.is_authenticated():
+        raise PermissionDenied
+
+    Message.objects.filter(user=user).delete()
+
+    if not request.is_ajax():
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER') or '/')
+    else:
+        return HttpResponse('')
+
+
+def message_mark_read(request, message_id):
+    user = get_user(request)
+    if not user.is_authenticated():
+        raise PermissionDenied
+
+    message = get_object_or_404(Message, user=user, pk=message_id)
+    message.read = True
+    message.save()
+
+    if not request.is_ajax():
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER') or '/')
+    else:
+        return HttpResponse('')
+
+
 def message_mark_all_read(request):
     user = get_user(request)
     if not user.is_authenticated():
         raise PermissionDenied
+
     Message.objects.filter(user=user).update(read=True)
     if not request.is_ajax():
         return HttpResponseRedirect(request.META.get('HTTP_REFERER') or '/')
