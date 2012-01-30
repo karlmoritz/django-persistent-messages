0f31820054d19ba6a1e47f8cffd141bb8331c553
Karl Moritz Hermann <karlmoritz.hermann@gmail.com>
2012-01-30 18:02:00 +0000

    Simple AJAX templates for inbox and Pop-Up notifications

--- persistent_messages/views.py
+++ persistent_messages/views.py
@@ -6,9 +6,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+from django.template.defaultfilters import linebreaksbr
 from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _
+
 
 def message_detail(request, message_id):
     user = get_user(request)
@@ -23,7 +25,7 @@ def message_detail(request, message_id):
       return render_to_response('persistent_messages/message/detail.html', {'message': message}, 
           context_instance=RequestContext(request))
     else:
-        return HttpResponse('')
+        return HttpResponse(linebreaksbr(message.message)) # mimetype='application/javascript'
     
 
 
